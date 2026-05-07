f63f35ec0fa15e15df4ae3bf6903fe87dd7d9e7d
salif modibo keita <salifmodibok@gmail.com>
2026-05-07 09:32:17 +0000

    Enregistrement sur les classes

--- src/org/odk/model/Planning.java
+++ src/org/odk/model/Planning.java
@@ -12,4 +12,60 @@ public class Planning {
 
     public Planning() {
     }
+
+    // Constructeur avec paramètres
+    public Planning(int id, String titre, Date dateCreation, List<String> sections, List<String> lignes) {
+        this.id = id;
+        this.titre = titre;
+        this.dateCreation = dateCreation;
+        this.sections = sections;
+        this.lignes = lignes;
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public List<String> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<String> sections) {
+        this.sections = sections;
+    }
+
+    public List<String> getLignes() {
+        return lignes;
+    }
+
+    public void setLignes(List<String> lignes) {
+        this.lignes = lignes;
+    }
+
+    // Méthode demandée
+    public void attribuer() {
+        System.out.println("Planning attribué avec succès : " + titre);
+    }
 }
