22e1dcd066d23d4105dba94fbbd438c0f2ac0512
Aissata <aissata28t@gmail.com>
2026-05-08 14:46:49 +0000

    classe progress

--- src/org/odk/repository/interfaces/ProgressionRepository.java
+++ src/org/odk/repository/interfaces/ProgressionRepository.java
@@ -1,7 +1,15 @@
 package org.odk.repository.interfaces;
 
+import java.util.List;
+
 import org.odk.model.Progression;
 
 public interface ProgressionRepository {
 Progression sauvegarder(Progression progress);
+List<Progression> afficher();
+
+//void modifierCommentaire(
+//        int id,
+//        String nouveauCommentaire
+//);
 }
--- src/org/odk/repository/jdbc/JdbcProgressionRepository.java
+++ src/org/odk/repository/jdbc/JdbcProgressionRepository.java
@@ -2,6 +2,9 @@ package org.odk.repository.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.odk.DatabaseConnection;
 import org.odk.model.Progression;
@@ -33,6 +36,109 @@ public class JdbcProgressionRepository implements ProgressionRepository {
 			System.err.println("Erreur lors de la sauvegarde de la progression : " + e.getMessage());
 		}
 		return progress;
+	}
+	
+	@Override
+	public List<Progression> afficher() {
+
+	    List<Progression> progressions =
+	            new ArrayList<>();
+
+	    String sql = "SELECT * FROM progression";
+
+	    try(
+
+	        Connection connection =
+	                DatabaseConnection.getConnection();
+
+	        PreparedStatement ps =
+	                connection.prepareStatement(sql);
+
+	        ResultSet rs =
+	                ps.executeQuery();
+
+	    ) {
+
+	        while(rs.next()) {
+
+	            int id = rs.getInt("id");
+
+	            java.sql.Date sqlDate =
+	                    rs.getDate("date_progression");
+
+	            boolean etat =
+	                    rs.getBoolean("etat");
+
+	            String commentaire =
+	                    rs.getString("commentaire");
+
+	            int objectifId =
+	                    rs.getInt("objectif_id");
+
+	            int actionId =
+	                    rs.getInt(
+	                            "action_quotidienne_id"
+	                    );
+
+	            // Création objet
+	            Progression progression =
+	                    new Progression(
+	                            sqlDate,
+	                            etat,
+	                            commentaire
+	                    );
+
+	            progression.setObjectif_id(
+	                    objectifId
+	            );
+
+	            progression.setAction_quotidienne_id(
+	                    actionId
+	            );
+
+	            progressions.add(progression);
+
+	            // Affichage
+	            System.out.println(
+	                    "ID : " + id
+	            );
+
+	            System.out.println(
+	                    "Date : " + sqlDate
+	            );
+
+	            System.out.println(
+	                    "Etat : " + etat
+	            );
+
+	            System.out.println(
+	                    "Commentaire : "
+	                    + commentaire
+	            );
+
+	            System.out.println(
+	                    "Objectif ID : "
+	                    + objectifId
+	            );
+
+	            System.out.println(
+	                    "Action ID : "
+	                    + actionId
+	            );
+
+	            System.out.println(
+	                    "----------------"
+	            );
+	        }
+
+	    } catch(Exception e) {
+
+	        System.out.println(
+	                e.getMessage()
+	        );
+	    }
+
+	    return progressions;
 	}
 
 }
