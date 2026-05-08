9a09056b2a95beacf9e750f556b511e2cca2e302
Idrissa Gassama <idrissag97@gmail.com>
2026-05-08 17:41:11 +0000

    modification 2

--- src/org/odk/model/Objectif.java
+++ src/org/odk/model/Objectif.java
@@ -4,90 +4,129 @@ import java.time.LocalDate;
 
 import org.odk.enums.EnumStatut;
 
-public abstract class Objectif {
-	protected int id;
-	protected int utilsateur_id;
-	protected String nom_objectif,description;
-	protected LocalDate date_debut,date_fin; 
-	protected EnumStatut status; 
-	
-	public Objectif() {
-		
-	}
-	
-	public Objectif(
-		int id,
-		String nom_objectif ,
-		int utilsateur_id,
-		String description,
-		LocalDate date_debut,
-		LocalDate date_fin,
-		EnumStatut status) {
-		this.id=id;
-		this.nom_objectif=nom_objectif;
-		this.utilsateur_id=utilsateur_id;
-		this.description=description;
-		this.date_debut=date_debut;
-		this.date_fin=date_fin;
-		this.status=status;
-	}
-	public Objectif(
-			String nom_objectif ,
-			int utilsateur_id,
-			String description,
-			LocalDate date_debut,
-			LocalDate date_fin,
-			EnumStatut status) {
-			this.nom_objectif=nom_objectif;
-			this.utilsateur_id=utilsateur_id;
-			this.description=description;
-			this.date_debut=date_debut;
-			this.date_fin=date_fin;
-			this.status=status;
-		}
-	public String getNom_objectif() {
-		return nom_objectif;
-	}
-	public void setNom_objectif(String nom_objectif) {
-		this.nom_objectif = nom_objectif;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public LocalDate getDate_debut() {
-		return date_debut;
-	}
-	public void setDate_debut(LocalDate date_debut) {
-		this.date_debut = date_debut;
-	}
-	public LocalDate getDate_fin() {
-		return date_fin;
-	}
-	public void setDate_fin(LocalDate date_fin) {
-		this.date_fin = date_fin;
-	}
-	public EnumStatut getStatus() {
-		return status;
-	}
-	public void setStatus(EnumStatut status) {
-		this.status = status;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getUtilsateur_id() {
-		return utilsateur_id;
-	}
-	public void setUtilsateur_id(int utilsateur_id) {
-		this.utilsateur_id = utilsateur_id;
-	}
-	
-	
 
+public abstract class Objectif {
+
+    protected int id;
+
+    protected int utilisateur_id;
+
+    protected String nom_objectif;
+
+    protected String description;
+
+    protected LocalDate date_debut;
+
+    protected LocalDate date_fin;
+
+    protected EnumStatut status;
+
+    public Objectif() {
+    }
+
+    public Objectif(
+            int id,
+            String nom_objectif,
+            int utilisateur_id,
+            String description,
+            LocalDate date_debut,
+            LocalDate date_fin,
+            EnumStatut status
+    ) {
+        this.id = id;
+        this.nom_objectif = nom_objectif;
+        this.utilisateur_id = utilisateur_id;
+        this.description = description;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+        this.status = status;
+    }
+
+    public Objectif(
+            String nom_objectif,
+            int utilisateur_id,
+            String description,
+            LocalDate date_debut,
+            LocalDate date_fin,
+            EnumStatut status
+    ) {
+        this.nom_objectif = nom_objectif;
+        this.utilisateur_id = utilisateur_id;
+        this.description = description;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+        this.status = status;
+    }
+
+    public abstract double calculerPourcentage();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUtilisateur_id() {
+        return utilisateur_id;
+    }
+
+    public void setUtilisateur_id(int utilisateur_id) {
+        this.utilisateur_id = utilisateur_id;
+    }
+
+    public String getNom_objectif() {
+        return nom_objectif;
+    }
+
+    public void setNom_objectif(String nom_objectif) {
+        this.nom_objectif = nom_objectif;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate_debut() {
+        return date_debut;
+    }
+
+    public void setDate_debut(LocalDate date_debut) {
+        this.date_debut = date_debut;
+    }
+
+    public LocalDate getDate_fin() {
+        return date_fin;
+    }
+
+    public void setDate_fin(LocalDate date_fin) {
+        this.date_fin = date_fin;
+    }
+
+    public EnumStatut getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumStatut status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+
+        return "\nObjectif {" +
+                "\n id = " + id +
+                "\n nom_objectif = '" + nom_objectif + '\'' +
+                "\n description = '" + description + '\'' +
+                "\n date_debut = " + date_debut +
+                "\n date_fin = " + date_fin +
+                "\n status = " + status +
+                "\n utilisateur_id = " + utilisateur_id +
+                "\n}";
+    }
 }
--- src/org/odk/model/ObjectifSport.java
+++ src/org/odk/model/ObjectifSport.java
@@ -3,30 +3,33 @@ package org.odk.model;
 import java.time.LocalDate;
 import org.odk.enums.EnumStatut;
 
-public class ObjectifSport extends Objectif{
+public class ObjectifSport extends Objectif {
 
+    private String type_activite;
+    private int duree_seance;
+    private int nb_seances_realisees;
+    private int frequence_hebdo;
+    private String niveauSportif;
 
-    protected String type_activite;
-    protected int duree_seance;
-    protected int nb_seances_realisees;
-    protected int frequence_hebdo;
-    protected String niveauSportif;
+    public ObjectifSport() {
+        super();
+    }
 
     public ObjectifSport(
-		int id, 
-		String nom_objectif, 
-		int utilsateur_id, 
-		String description, 
-		LocalDate date_debut, 
-		LocalDate date_fin, 
-		EnumStatut status,
-
-		int duree_seance, 
-		int frequence_hebdo, 
-		int nb_seances_realisees, 
-		String niveauSportif, 
-		String type_activite) {
-        super(id, nom_objectif, utilsateur_id, description, date_debut, date_fin, status);
+            int id,
+            String nom_objectif,
+            int utilisateur_id,
+            String description,
+            LocalDate date_debut,
+            LocalDate date_fin,
+            EnumStatut status,
+            int duree_seance,
+            int frequence_hebdo,
+            int nb_seances_realisees,
+            String niveauSportif,
+            String type_activite
+    ) {
+        super(id, nom_objectif, utilisateur_id, description, date_debut, date_fin, status);
         this.duree_seance = duree_seance;
         this.frequence_hebdo = frequence_hebdo;
         this.nb_seances_realisees = nb_seances_realisees;
@@ -34,44 +37,54 @@ public class ObjectifSport extends Objectif{
         this.type_activite = type_activite;
     }
 
-	public String getType_activite() {
-		return type_activite;
-	}
+    @Override
+    public double calculerPourcentage() {
+        if (frequence_hebdo <= 0) {
+            return 0;
+        }
 
-	public void setType_activite(String type_activite) {
-		this.type_activite = type_activite;
-	}
+        double pourcentage = ((double) nb_seances_realisees / frequence_hebdo) * 100;
 
-	public int getDuree_seance() {
-		return duree_seance;
-	}
+        return Math.min(pourcentage, 100);
+    }
 
-	public void setDuree_seance(int duree_seance) {
-		this.duree_seance = duree_seance;
-	}
+    public String getType_activite() {
+        return type_activite;
+    }
 
-	public int getNb_seances_realisees() {
-		return nb_seances_realisees;
-	}
+    public void setType_activite(String type_activite) {
+        this.type_activite = type_activite;
+    }
 
-	public void setNb_seances_realisees(int nb_seances_realisees) {
-		this.nb_seances_realisees = nb_seances_realisees;
-	}
+    public int getDuree_seance() {
+        return duree_seance;
+    }
 
-	public int getFrequence_hebdo() {
-		return frequence_hebdo;
-	}
+    public void setDuree_seance(int duree_seance) {
+        this.duree_seance = duree_seance;
+    }
 
-	public void setFrequence_hebdo(int frequence_hebdo) {
-		this.frequence_hebdo = frequence_hebdo;
-	}
+    public int getNb_seances_realisees() {
+        return nb_seances_realisees;
+    }
 
-	public String getNiveauSportif() {
-		return niveauSportif;
-	}
+    public void setNb_seances_realisees(int nb_seances_realisees) {
+        this.nb_seances_realisees = nb_seances_realisees;
+    }
 
-	public void setNiveauSportif(String niveauSportif) {
-		this.niveauSportif = niveauSportif;
-	}
+    public int getFrequence_hebdo() {
+        return frequence_hebdo;
+    }
 
+    public void setFrequence_hebdo(int frequence_hebdo) {
+        this.frequence_hebdo = frequence_hebdo;
+    }
+
+    public String getNiveauSportif() {
+        return niveauSportif;
+    }
+
+    public void setNiveauSportif(String niveauSportif) {
+        this.niveauSportif = niveauSportif;
+    }
 }
